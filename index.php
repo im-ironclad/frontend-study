bd762a4067f4c9d74d9fd93439978bbe2b357d21
Adam Wells <adam@interactiveknowledge.com>
2019-01-31 19:28:11 -0500

    Added some stuff to index.php

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
-  phpinfo();
+  # TODO: Make activityLink structure dynamic to all folders inside the base Activity directory
+
+  $dirResult = scandir('./');
+  $activityLinks = [];
+
+  forEach($dirResult as $thing) {
+    if ($thing === 'Activities') {
+      array_push($activityLinks, $thing);
+    }
+  }
+
+  echo $activityLinks[0];
 
 ?>
